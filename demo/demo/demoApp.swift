adb0a4d27bf6cf9081ba5825e864df17be828810
Darien <r880514@gmail.com>
2021-05-22 21:32:29 +0800

    init

---
+++ demo/demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Akira Akimoto on 2021/5/22.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
