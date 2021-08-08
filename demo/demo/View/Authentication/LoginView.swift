ea44ea44ddff88b9f2b14888d21b883745a2cd29
Darien <r880514@gmail.com>
2021-08-08 21:07:09 +0800

    註冊頁面

--- demo/demo/View/Authentication/LoginView.swift
+++ demo/demo/View/Authentication/LoginView.swift
@@ -12,66 +12,72 @@ struct LoginView: View {
     @State var password = ""
     
     var body: some View {
-        ZStack {
-            VStack {
-                Image("twitter-logo")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 220, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-                    .padding(.top, 88)
-                    .padding(.bottom, 32)
-                
-                VStack (spacing: 20) {
-                    CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
-                        .padding()
-                        .background(Color(.init(white: 1, alpha: 0.15)))
-                        .cornerRadius(10)
-                        .foregroundColor(.white)
+        NavigationView {
+            ZStack {
+                VStack {
+                    Image("twitter-logo")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 220, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                        .padding(.top, 88)
+                        .padding(.bottom, 32)
+                    
+                    VStack (spacing: 20) {
+                        CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
+                            .padding()
+                            .background(Color(.init(white: 1, alpha: 0.15)))
+                            .cornerRadius(10)
+                            .foregroundColor(.white)
+                            
+                        CustomSecureField(text: $password, placeholder: Text("Password"))
+                            .padding()
+                            .background(Color(.init(white: 1, alpha: 0.15)))
+                            .cornerRadius(10)
+                            .foregroundColor(.white)
+                    }
+                    .padding(.horizontal, 32)
+                    
+                    HStack {
+                        Spacer()
                         
-                    CustomSecureField(text: $password, placeholder: Text("Password"))
-                        .padding()
-                        .background(Color(.init(white: 1, alpha: 0.15)))
-                        .cornerRadius(10)
-                        .foregroundColor(.white)
-                }
-                .padding(.horizontal, 32)
-                
-                HStack {
-                    Spacer()
+                        Button(action: { }, label: {
+                            Text("Forgot Password?")
+                                .font(.footnote)
+                                .bold()
+                                .foregroundColor(.white)
+                                .padding(.top, 16)
+                                .padding(.trailing, 32)
+                        })
+                    }
                     
                     Button(action: { }, label: {
-                        Text("Forgot Password?")
-                            .font(.footnote)
-                            .bold()
-                            .foregroundColor(.white)
-                            .padding(.top, 16)
-                            .padding(.trailing, 32)
+                        Text("Sign In")
+                            .font(.headline)
+                            .foregroundColor(.blue)
+                            .frame(width: 360, height: 50)
+                            .background(Color.white)
+                            .clipShape(Capsule())
+                            .padding()
                     })
+                    
+                    Spacer()
+                    
+                    NavigationLink(
+                        destination: RegistrationView().navigationBarBackButtonHidden(true),
+                        label: {
+                            HStack {
+                                Text("Don't have an account?")
+                                Text("Sign Up")
+                                    .font(.system(size: 14, weight: .semibold))
+                            }
+                            .foregroundColor(.white)
+                            .padding(.bottom, 40)
+                        })
                 }
-                
-                Button(action: { }, label: {
-                    Text("Sign In")
-                        .font(.headline)
-                        .foregroundColor(.blue)
-                        .frame(width: 360, height: 50)
-                        .background(Color.white)
-                        .clipShape(Capsule())
-                        .padding()
-                })
-                
-                Spacer()
-                
-                HStack {
-                    Text("Don't have an account?")
-                    Text("Sign Up")
-                        .font(.system(size: 14, weight: .semibold))
-                }
-                .foregroundColor(.white)
-                .padding(.bottom, 40)
             }
+            .background(Color(#colorLiteral(red: 0.1068081036, green: 0.6316717267, blue: 0.9511808753, alpha: 1))) // Color(Color Literal) 選色器
+            .ignoresSafeArea()
         }
-        .background(Color(#colorLiteral(red: 0.1068081036, green: 0.6316717267, blue: 0.9511808753, alpha: 1))) // Color(Color Literal) 選色器
-        .ignoresSafeArea()
         
     }
 }
--- demo/demo/demoApp.swift
+++ demo/demo/demoApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct demoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            LoginView()
         }
     }
 }
