9b2abaf788d001d87adf316ae24e97f35c3149ea
Darien <r880514@gmail.com>
2021-08-22 21:58:33 +0800

    check follow function

--- demo/demo/ViewModels/ProfileViewModel.swift
+++ demo/demo/ViewModels/ProfileViewModel.swift
@@ -14,6 +14,7 @@ class ProfileViewModel: ObservableObject {
 
     init(user: User) {
         self.user = user
+        checkIfUserIsFollowed()
     }
     
     func follow() {
@@ -38,6 +39,16 @@ class ProfileViewModel: ObservableObject {
             followersRef.document(currentUid).delete { _ in
                 self.isFollowed = false
             }
+        }
+    }
+    
+    func checkIfUserIsFollowed() {
+        guard let currentUid = Auth.auth().currentUser?.uid else { return }
+        let followingRef = COLLECTION_FOLLOWING.document(currentUid).collection("user-following")
+        
+        followingRef.document(user.id).getDocument { snapshot, _ in
+            guard let isFollowed = snapshot?.exists else { return }
+            self.isFollowed = isFollowed
         }
     }
 }
